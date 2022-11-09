28241a2b1e2a25ff31f70e2a6d97bf78669c7a27
MateoBohorquezUniandes <i.bohorquezp@uniandes.edu.co>
2022-11-09 12:41:41 -0500

    Pruebas basicas

--- Kraken/features/my_first.feature
+++ Kraken/features/my_first.feature
@@ -3,6 +3,10 @@ Feature: My feature
 @user1 @web
 Scenario: Como primer usuario inicio sesión
   Given I navigate to page "http://www.google.com"
+  And I wait for 5 seconds
+  When I enter text to search 'Uniandes'
+  And I click on search button
+  And I wait for 5 seconds
 
 @user2 @web
 Scenario: Como segundo usuario inicio sesión
--- Kraken/features/web/step_definitions/step.js
+++ Kraken/features/web/step_definitions/step.js
@@ -1,1 +1,9 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
+When('I enter text to search {string}',async function(search){
+    let element = await this.driver.$('.gLFyf');
+    return await element.setValue(search);
+});
+When('I click on search button', async function(){
+    let element = await this.driver.$('.gNO89b');
+    return await element.click();
+});
